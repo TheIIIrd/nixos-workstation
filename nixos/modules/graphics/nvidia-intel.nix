21e79577a1564e90bd46b20b430f1f0cb6f7b582
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2025-07-27 14:14:35 +0300

    Add graphics configs

---
+++ nixos/modules/graphics/nvidia-intel.nix
@@ -0,0 +1,43 @@
+{ pkgs, config, ... }: {
+  hardware.graphics = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      cudaPackages.cudatoolkit
+      egl-wayland
+      intel-media-driver
+      nvidia-vaapi-driver
+      vpl-gpu-rt
+    ];
+  };
+
+  services.xserver.videoDrivers = [
+    "modesetting"  # Example for Intel iGPU; use "amdgpu" here instead if your iGPU is AMD
+    "nvidia"
+  ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = true;
+    nvidiaSettings = true;
+
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      # Specify the Bus IDs for your GPUs; make sure they match your system's hardware
+      # sudo lshw -c display
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+}
---
+++ nixos/modules/graphics/nvidia.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, ... }: {
+  hardware.graphics = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      cudaPackages.cudatoolkit
+      egl-wayland
+      nvidia-vaapi-driver
+    ];
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = true;
+    nvidiaSettings = true;
+
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+}
