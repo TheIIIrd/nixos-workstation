2f26ce1102f2463e005b0b5c8bb1a5042dc37a27
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2025-09-15 08:41:19 +0300

    Update wireguard.nix

--- nixos/modules/network/wireguard.nix
+++ nixos/modules/network/wireguard.nix
@@ -1,25 +1,22 @@
 {
-  networking.wireguard = {
-    enable = true;
-    interfaces = {
-      wg0 = {
-        ips = [ "10.0.0.2/24" ];
-        listenPort = 51820;
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [ "10.0.0.2/24" ];
+      # dns = [ "DNS_SERVER_IP" ];
 
-        # privateKeyFile = "PATH_TO_PRIVATE_KEY_FILE";
-        privateKey = "CLIENT_PRIVATE_KEY";
+      # privateKeyFile = "PATH_TO_PRIVATE_KEY_FILE";
+      privateKey = "CLIENT_PRIVATE_KEY";
 
-        peers = [
-          {
-            publicKey = "SERVER_PUBLIC_KEY";
+      peers = [
+        {
+          publicKey = "SERVER_PUBLIC_KEY";
 
-            allowedIPs = [ "0.0.0.0/0" ];
-            endpoint = "SERVER_IP:51820";
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+          endpoint = "SERVER_IP:51820";
 
-            persistentKeepalive = 25;
-          }
-        ];
-      };
+          persistentKeepalive = 25;
+        }
+      ];
     };
   };
 }
